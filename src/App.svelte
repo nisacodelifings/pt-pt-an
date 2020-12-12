50cc4f0b70313fe67ce5897af4205849a422a6b5
nisacodelifings <annisap.siswa@smkn4bdg.sch.id>
2020-12-11 20:01:10 -0800

    mantap

---
+++ src/App.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Features from "./components/Features.svelte";
+  import Container from "./components/Container.svelte";
+</script>
+
+<body class="mdl-demo mdl-color-text--white-700 mdl-base">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-color--blue-900">
+      <Header />
+      <main class="mdl-layout__content">
+        <div class="mdl-layout__tab-panel is-active" id="overview">
+          <Container />
+          <Container />
+          <Container />
+          <Container />
+          <Container />
+          <Container />
+          <Container />
+        </div>
+        <br>
+        <Features />
+        <footer class="mdl-mega-footer">
+          <div class="mdl-mega-footer--middle-section">
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">Features</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Terms</a></li>
+                <li><a href="#">Partners</a></li>
+                <li><a href="#">Updates</a></li>
+              </ul>
+            </div>
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">Details</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">Spec</a></li>
+                <li><a href="#">Tools</a></li>
+                <li><a href="#">Resources</a></li>
+              </ul>
+            </div>
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">Technology</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">How it works</a></li>
+                <li><a href="#">Patterns</a></li>
+                <li><a href="#">Usage</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Contracts</a></li>
+              </ul>
+            </div>
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">FAQ</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">Questions</a></li>
+                <li><a href="#">Answers</a></li>
+                <li><a href="#">Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="mdl-mega-footer--bottom-section">
+            <div class="mdl-logo">
+              More Information
+            </div>
+            <ul class="mdl-mega-footer--link-list">
+              <li><a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a></li>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Privacy and Terms</a></li>
+            </ul>
+          </div>
+        </footer>
+      </main>
+    </div>
+    <a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/text-only/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">View Source</a>
+  </body>
---
+++ src/components/Container.svelte
@@ -0,0 +1,16 @@
+<script>
+  let judul = 'Pembuatan Dus Kemasan'
+  let isi = 'Cape jadi Reseller? Ingin memiliki produk anda sendiri? atau Ingin membuat produk anda menjadi lebih terpercaya dengan adanya box kemasan? Kami dapat membantu anda membuat berbagai macam dus kemasan untuk produk kecantikan dan lain-lain.'
+</script>
+
+<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+            <div class="mdl-card mdl-cell mdl-cell--12-col mdl-color--blue-700">
+              <div class="mdl-card__supporting-text">
+                <h4>{judul}</h4>
+                {isi}
+              </div>
+              <div class="mdl-card__actions mdl-color--blue-800">
+                <a href="#" class="mdl-button">Order</a>
+              </div>
+            </div>
+          </section>
